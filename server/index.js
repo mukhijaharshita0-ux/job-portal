1f4a2a764374d47d8e12338f9619ea7bfa813487
mukhijaharshita0-ux <mukhijaharshita0@gmail.com>
2026-01-27 12:03:21 +0530

    Fix CORS by removing credentials

--- server/index.js
+++ server/index.js
@@ -34,19 +34,14 @@ app.use(express.urlencoded({ extended: false }));
 //   allowedHeaders: ["Content-Type", "Authorization"],
 //   credentials: true
 // }));
-
 app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://job-portal-v8lt.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
-
-// important for preflight requests
-app.options("*", cors());
 
 
 
